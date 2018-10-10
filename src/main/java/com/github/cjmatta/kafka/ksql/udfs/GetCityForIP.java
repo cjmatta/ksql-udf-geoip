761f1fa838055c6f259db5e4e2d791e4bb82038d
Chris Matta <chris@confluent.io>
2018-10-10 14:23:27 -0400

    reorganized for static initialization, plus made the DB location a jvm property

--- src/main/java/com/github/cjmatta/kafka/ksql/udfs/GetCityForIP.java
+++ src/main/java/com/github/cjmatta/kafka/ksql/udfs/GetCityForIP.java
@@ -15,25 +15,34 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 import java.util.Map;
 
 @UdafDescription(name="geoip_lookup", description = "Function to lookup ip -> city information ")
 public class GetCityForIP {
-    @Udf(description = "returns city from IP input")
-    public String getcityforip(String ip) {
-      try {
-        ClassLoader classLoader = this.getClass().getClassLoader();
-        File database = new File(classLoader.getResource("GeoLite2-City_20181009/GeoLite2-City.mmdb").toURI());
-        DatabaseReader reader = new DatabaseReader.Builder(database).build();
+  private ClassLoader classLoader = this.getClass().getClassLoader();
+  private static File database;
+  private static DatabaseReader reader;
 
-        InetAddress ipAddress = InetAddress.getByName(ip);
-
-        CityResponse response = reader.city(ipAddress);
-        City city = response.getCity();
-        return city.getName();
-      } catch (IOException | GeoIp2Exception | URISyntaxException e) {
-        System.out.println(e.toString());
-        return null;
-      }
+  static {
+    try {
+      database = new File(System.getProperty("geolite.db.path"));
+      reader = new DatabaseReader.Builder(database).build();
+    } catch (IOException e) {
+      System.out.println(e.toString());
+      throw new ExceptionInInitializerError(e);
+    }
+  }
+  
+  @Udf(description = "returns city from IP input")
+  public String getcityforip(String ip) {
+    try {
+      InetAddress ipAddress = InetAddress.getByName(ip);
+      CityResponse response = reader.city(ipAddress);
+      City city = response.getCity();
+      return city.getName();
+    } catch (IOException | GeoIp2Exception e) {
+      return null;
+    }
   }
 }
